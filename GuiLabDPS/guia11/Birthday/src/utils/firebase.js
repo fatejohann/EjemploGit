1040271195cde019249e826e30461d70676253f1
fatejohann <holaquepasa70@gmail.com>
2024-04-26 08:41:44 -0600

    guia 11

---
+++ GuiLabDPS/guia11/Birthday/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmfYELm4MKKtI3Ad1Z32uHhmCsUytO7Lk",
+  authDomain: "birthday-931d7.firebaseapp.com",
+  projectId: "birthday-931d7",
+  storageBucket: "birthday-931d7.appspot.com",
+  messagingSenderId: "440631388928",
+  appId: "1:440631388928:web:024a5b643781871e3aa1e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
